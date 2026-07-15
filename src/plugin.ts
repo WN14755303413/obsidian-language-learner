e73bc67d196fb63e15dab0549ae9300e01fbb710
guopenghui <guopeng_hui@126.com>
2026-07-15 21:09:13 +0800

    删除已弃用的pdf功能

--- src/plugin.ts
+++ src/plugin.ts
@@ -18,7 +18,6 @@ import { READING_VIEW_TYPE, READING_ICON, ReadingView } from "./views/ReadingVie
 import { LearnPanelView, LEARN_ICON, LEARN_PANEL_VIEW } from "./views/LearnPanelView";
 import { StatView, STAT_ICON, STAT_VIEW_TYPE } from "./views/StatView";
 import { DataPanelView, DATA_ICON, DATA_PANEL_VIEW } from "./views/DataPanelView";
-// import { PDFView, PDF_FILE_EXTENSION, VIEW_TYPE_PDF } from "./views/PDFView";
 
 import { t } from "./lang/helper";
 import DbProvider from "./db/base";
@@ -88,8 +87,6 @@ export default class LanguageLearner extends Plugin {
         // 	callback: () => new Notice("hello!")
         // })
 
-        // await this.replacePDF();
-
         this.initStore();
 
         this.addCommands();
@@ -121,9 +118,6 @@ export default class LanguageLearner extends Plugin {
 
         this.db.close();
         this.server?.close();
-        // if (await app.vault.adapter.exists(".obsidian/plugins/obsidian-language-learner/pdf/web/viewer.html")) {
-        //     this.registerExtensions([PDF_FILE_EXTENSION], "pdf");
-        // }
 
         this.vueApp.unmount();
         this.appEl.remove();
@@ -136,28 +130,6 @@ export default class LanguageLearner extends Plugin {
             platform: Platform.isMobile ? "mobile" : "desktop",
         };
     }
-
-    // async replacePDF() {
-    //     if (await app.vault.adapter.exists(
-    //         ".obsidian/plugins/obsidian-language-learner/pdf/web/viewer.html"
-    //     )) {
-    //         this.registerView(VIEW_TYPE_PDF, (leaf) => {
-    //             return new PDFView(leaf);
-    //         });
-
-    //         (this.app as any).viewRegistry.unregisterExtensions([
-    //             PDF_FILE_EXTENSION,
-    //         ]);
-    //         this.registerExtensions([PDF_FILE_EXTENSION], VIEW_TYPE_PDF);
-
-    //         this.registerDomEvent(window, "message", (evt) => {
-    //             if (evt.data.type === "search") {
-    //                 // if (evt.data.funckey || this.store.searchPinned)
-    //                 this.queryWord(evt.data.selection);
-    //             }
-    //         });
-    //     }
-    // }
 
     initStore() {
         this.store.dark = document.body.hasClass("theme-dark");
